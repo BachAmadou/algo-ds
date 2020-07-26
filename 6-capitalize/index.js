49c77fcf29f941301d9ed94969bc45fbc15ab46c
Bach <bafosth@yahoo.fr>
2020-07-26 11:44:20 -0400

    added capitalize project

---
+++ 6-capitalize/index.js
@@ -0,0 +1,54 @@
+// Write a function that returns the provided string with
+// the first letter of each word capitalized. Make sure the
+// rest of the word is in lower case.
+// --- Examples
+//   capitalize("I'm a little tea pot") --> 'I'm A Little Tea Pot'
+//   capitalize('sHoRt AnD sToUt') --> 'Short And Stout'
+
+function capitalize(str) {
+  const strWords = str.split(" ");
+  const wordsCase = [];
+
+  for(let i = 0; i < strWords.length; i++) {
+    const letter = strWords[i];
+    wordsCase.push(letter[0].toUpperCase() + letter.slice(1).toLowerCase());
+  }
+  return wordsCase.join(" ");
+}
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("Capitalization", () => {
+  it("capitalizes the first letter of each word", () => {
+    assert.equal(capitalize("I'm a little tea pot"), "I'm A Little Tea Pot");
+  });
+  it("lowercases the rest of the word", () => {
+    assert.equal(capitalize("sHoRt AnD sToUt"), "Short And Stout");
+  });
+});
+
+mocha.run();
