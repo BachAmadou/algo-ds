4279dae6e56824f5557ab725f8d097997bde48ca
Bach <bafosth@yahoo.fr>
2020-07-28 11:56:44 -0400

    added vowels project

--- 12-vowels/index.js
+++ 12-vowels/index.js
@@ -1,32 +1,31 @@
-// Given a phrase, substitute each character by shifting it up
-// the alphabet by a given integer. If necessary, the shifting
-// should wrap around back to the beginning of the alphabet.
-// Make sure the function only shifts letters.
+// Write a function that returns the number of vowels found within a string.
+// Vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+// Make sure the function is case insensitive!
 // --- Examples
-// caeserCipher("abcd", 1) === "bcde";
-// caeserCipher("yz", 1) === "za";
-// caeserCipher("abcd", 100) === "wxyz";
-// caeserCipher("gurer ner 9 qbtf!", 13) === "there are 9 dogs!"
+//   vowels('What') --> 1
+//   vowels('Why?') --> 0
+//   vowels('aEiOu') --> 5
+//   vowels('I am a world-class developer using iterations') --> 16
 
-// LINK TO CODEPEN VERSION: https://codepen.io/816978819/pen/MLrVor
+// LINK TO CODEPEN VERSION: https://codepen.io/816978819/pen/ZwOPaR
 
-function caesarCipher(str, shift) {
-  const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("");
-  let res = "";
+function vowels(str) {
+  const vowels = "aeiou".split("");
+  str = str.toLowerCase();
+  let count = 0;
 
   for(let i = 0; i < str.length; i++) {
     const char = str[i];
-    const index = alphabetArr.indexOf(char);
 
-    if(index === -1) {
-      res += char;
-      continue; // what the "continue" keyword does is, it will not allow anymore code to run after "coutinue"
+    for(let j = 0; j < vowels.length; j++) {
+      const letter = vowels[j];
+
+      if(char === letter) {
+        count++;
+      }
     }
-
-    const codedIdx = (index + shift) % 26;
-    res += alphabetArr[codedIdx];
   }
-  return res;
+  return count;
 }
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
@@ -55,14 +54,14 @@ function caesarCipher(str, shift) {
 mocha.setup("bdd");
 const { assert } = chai;
 
-describe("caesarCipher()", () => {
-  it("shifts letters correctly.", () => {
-    assert.equal(caesarCipher("abcd", 1), "bcde");
-    assert.equal(caesarCipher("yz", 1), "za");
-    assert.equal(caesarCipher("abcd", 100), "wxyz");
+describe("Vowels", () => {
+  it("gets correct vowel count", () => {
+    assert.equal(vowels("What"), 1);
+    assert.equal(vowels("Why"), 0);
   });
-  it("does not shift digits or other characters such as '!'.", () => {
-    assert.equal(caesarCipher("gurer ner 9 qbtf!", 13), "there are 9 dogs!");
+  it("is case insensitive. vowels('aEiOu') should return 5", () => {
+    assert.equal(vowels("aEiOu"), 5);
+    assert.equal(vowels("I am a world-class developer using iterations"), 16);
   });
 });
 
