8f1937600086686b866f41d31912b1144e95bfdc
Bach <bafosth@yahoo.fr>
2020-08-03 21:03:35 -0400

    added projects of sorting and merge, moved all the algo projects in a new folder, and added an algo cheatsheet

---
+++ algorithms/4-maxChar/index.js
@@ -0,0 +1,63 @@
+// Return the character most commonly used in the string.
+// --- Examples
+// maxChar("I loveeeeeee noodles") === "e"
+// maxChar("1337") === "3"
+
+// LINK TO CODEPEN VERSION: https://codepen.io/816978819/pen/RvaEox
+
+function maxChar(str) {
+  let charCountObj = {};
+  let maxChar = "";
+  let maxCount = 0;
+
+  // Fill out character count object
+  for(let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    charCountObj[char] = charCountObj[char] + 1 || 1; // the character count object has the letter yet, if it does not then count it 1. Or if it does exist already then increse it to 2 or 3 etc. 
+
+    // Updating the variables of maxChar and maxCount
+    if(charCountObj[char] > maxCount) {
+      maxChar = char;
+      maxCount = charCountObj[char];
+    }
+  }
+  return maxCount;
+}
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("Max Character", () => {
+  it("maxChar() finds the most frequently used character", () => {
+    assert.equal(maxChar("a"), "a");
+    assert.equal(maxChar("test"), "t");
+    assert.equal(maxChar("I loveeeeee noodles"), "e");
+    assert.equal(maxChar("1337"), "3");
+  });
+});
+
+mocha.run();
