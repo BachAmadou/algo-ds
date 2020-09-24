312c485258b4b4027226b9c4c8caff81670c4d91
BachAmadou <bafosth@yahoo.fr>
2020-09-24 18:48:59 -0400

    added dynamic programming folder

--- strings/LC3_lengthOfLongestSubstring/index.js
+++ strings/LC3_lengthOfLongestSubstring/index.js
@@ -1,3 +1,9 @@
+//Given a string find the length of the longest substring(word from the original string)
+// without repeating characters
+// Good explanation video, used the solution: (https://www.youtube.com/watch?v=WKTgajDkVcA)
+// Note: solution will be: to declare a start index and keep updating the index by looping
+// through an array using 2 pointers for left and rigth
+
 function lengthOfLongestSubstring(s) {}
 
 module.exports = lengthOfLongestSubstring;
