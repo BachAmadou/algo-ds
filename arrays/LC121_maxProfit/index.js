d6bda31b6729caa83d61575fe43b0f4dc3624252
BachAmadou <bafosth@yahoo.fr>
2020-09-29 21:46:31 -0400

    added script the maxProfit project

--- arrays/LC121_maxProfit/index.js
+++ arrays/LC121_maxProfit/index.js
@@ -1,3 +1,19 @@
-function maxProfit(prices) {}
+function maxProfit(prices) {
+
+    // SOLUTION 1:
+    let maxProfit = 0;
+    let cheapestPrice = prices[0];
+
+    for(const currentPrice of prices) {
+        if(currentPrice < cheapestPrice) {
+            cheapestPrice = currentPrice;
+        }
+
+        const currentProfit = currentPrice - cheapestPrice;
+        maxProfit = Math.max(currentProfit, maxProfit);
+    }
+
+    return maxProfit;
+}
 
 module.exports = maxProfit;
