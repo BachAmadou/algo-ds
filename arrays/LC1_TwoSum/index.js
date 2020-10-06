c64656b6984e9065dba47155935a7b3b97cee638
BachAmadou <bafosth@yahoo.fr>
2020-10-06 19:19:45 -0400

    added script solution 1 to twoSum project

--- arrays/LC1_TwoSum/index.js
+++ arrays/LC1_TwoSum/index.js
@@ -1,4 +1,18 @@
 
-function twoSum(arr, target) {}
+function twoSum(arr, target) {
+    // SOLUTION 1
+    const numsVisited = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        const complement = target - num;
+
+        if (complement in numsVisited) {
+            return [i, numsVisited[complement]];
+        } else {
+            numsVisited[num] = i;
+        }
+    }
+}
 
 module.exports = twoSum;
